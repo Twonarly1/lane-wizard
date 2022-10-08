38bbbca91caa0099685422a0abf7b78d3af299c5
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2022-10-08 18:08:03 -0500

    feat(auth): add next auth session provider

---
+++ lib/env.ts
@@ -0,0 +1,11 @@
+/**
+ * Environment variables.
+ * Exporting destructured `process.env` does not work in Next.js without hacking:
+ * @see https://nextjs.org/docs/api-reference/next.config.js/environment-variables
+ * @see https://github.com/vercel/next.js/issues/15574
+ */
+
+// core
+export const NODE_ENV = process.env.NODE_ENV
+export const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID!
+export const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET!
---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,14 @@
+import NextAuth from "next-auth"
+import GoogleProvider from "next-auth/providers/google"
+import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } from "../../../lib/env"
+
+export const authOptions = {
+    // Configure one or more authentication providers
+    providers: [
+        GoogleProvider({
+            clientId: GOOGLE_CLIENT_ID,
+            clientSecret: GOOGLE_CLIENT_SECRET,
+        }),
+    ],
+}
+export default NextAuth(authOptions)
