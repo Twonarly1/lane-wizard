cdb11d9f5d9ee29479b5b20dc1d7dd860c5cb428
Beau Hawkinson <hawkinson.beau@yahoo.com>
2022-10-10 13:44:17 -0500

    next/auth

--- lib/env.ts
+++ lib/env.ts
@@ -9,3 +9,4 @@
 export const NODE_ENV = process.env.NODE_ENV
 export const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID!
 export const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET!
+export const NEXTAUTH_SECRET = process.env.NEXTAUTH_SECRET!
